157b19fc19e28228e781eb76f63adce36c76643e
Ricardolpz99 <rilo.c@hotmail.es>
2022-01-21 03:48:44 -0600

    Adding the election audit code.

---
+++ Practice/for practice.py
@@ -0,0 +1,5 @@
+numbers = [0, 1, 2, 3, 4]
+for num in range(5):
+    print(num)
+    
+    
---
+++ Python_practice.py
@@ -0,0 +1,6 @@
+counties = ["Arapahoe","Denver","Jefferson"]
+counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
+for county, voters in counties_dict.items():
+    print(f"{county} county has {voters:,} registered voters.")
+>>> import csv
+>>> dir(csv)
